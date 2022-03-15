8263cc6d2c83c39ad377bd1884a7eec084a61845
leonphoenix21 <scamonoel169@gmail.com>
2022-03-15 10:48:33 -0500

    splash Page Css fixed title

--- express-project-starter/routes/application.js
+++ express-project-starter/routes/application.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET application page. */
 //? /application by default because of file name
 router.get('/', (req, res) => {
-  res.render('application', { title: 'a/A Express Skeleton Home ==> Application' });
+  res.render('application', { title: 'application' });
 });
 
 module.exports = router;
