2225df6061f0a5a9064d0cb0eb4f88ac073139c3
leonphoenix21 <scamonoel169@gmail.com>
2022-03-17 18:51:26 -0500

    Task Order Fixed, Task Edit Button Fixed, and Edit List Fixed

---
+++ routes/editListRoute.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db/models');
+
+const { csrfProtection, asyncHandler } = require('./utils');
+//! csrfProtection only when we need to re-render a page don't dlt for bhavik :(
+
+router.post('/', asyncHandler(async (req, res) => {
+    const { name, listId } = req.body;
+
+    const editList = await db.List.findByPk(listId);
+
+    console.log('🍊🍊🍊🍊🍊🍊🍊🍊🍊🍊');
+    console.log(editList)
+
+    //Save is for updating
+    await editList.update({ name })
+
+    res.redirect('/application')
+}))
+
+
+module.exports = router;
