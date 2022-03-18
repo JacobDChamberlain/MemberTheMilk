1340d8c7ac23224f735ae6a3a5fa828d9adf7c66
bhavik <hyruka98@gmail.com>
2022-03-18 15:09:17 -0700

    Some styling on the application page finished

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -13,100 +13,97 @@ window.addEventListener("load", (event) => {
 
   // !111111111!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   //* Task Button CREATE AND ADD
-  const addTaskButton = document.getElementById('addTaskBtn');
-  //addTask Submit Button Event Listener
-  addTaskButton.addEventListener('click', e => {
-    //start with the div
-    e.preventDefault();
-    //append elements onto the div to make adding dynamic
-    //Checkbox,  Edit Button, Trash Button
-    const task = document.getElementById('addTaskInput');
-    const editTaskForm = document.getElementById('editTaskForm');
+  // const addTaskButton = document.getElementById('addTaskBtn');
+  // //addTask Submit Button Event Listener
+  // addTaskButton.addEventListener('click', e => {
+  //   //start with the div
+  //   e.preventDefault();
+  //   //append elements onto the div to make adding dynamic
+  //   //Checkbox,  Edit Button, Trash Button
+  //   const task = document.getElementById('addTaskInput');
+  //   const editTaskForm = document.getElementById('editTaskForm');
 
-    //If 
-    if (task.value) {
-      const currTaskRow = document.createElement('li');
-      const taskDisplay = document.querySelector('#taskDisplay');
+  //   //If 
+  //   if (task.value) {
+  //     const currTaskRow = document.createElement('li');
+  //     const taskDisplay = document.querySelector('#taskDisplay');
 
-      const checkbox = document.getElementById('checkbox');
-      checkbox.style.border = '3px'; //TODO FIX ME
+  //     const checkbox = document.getElementById('checkbox');
+  //     checkbox.style.border = '3px'; //TODO FIX ME
 
-      const editButton = document.createElement('button')
-      editButton.addEventListener('click', e => {
+  //     const editButton = document.createElement('button')
+  //     editButton.addEventListener('click', e => {
 
-        editTaskForm.style.visibility = 'visible'
-        const inputField = document.getElementById('taskRenameInputField');
-        const submitButton = document.getElementById('cancelButton');
-        const cancelButton = document.getElementById('submitButton');
-
-
-        // inputField.setAttribute('type', '')
-        //Submit Button
-        submitButton.innerText = 'Submit';
-        cancelButton.innerText = 'cancel';
-        submitButton.addEventListener('click', e => {
-
-          if (inputField.value) {
-            editTaskForm.style.visibility = 'hidden';
-            currTaskRow.appendChild(checkbox);
-            currTaskRow.innerText = inputField.value;
-            currTaskRow.appendChild(editButton).innerHTML = "Edit";
-            currTaskRow.appendChild(trashButton).innerText = "Trash";
-          } else {
-            inputField.placeholder = 'Type an input to submit form'
-          }
-
-        })
-
-        //Cancel Button
-        cancelButton.addEventListener('click', e => {
-          editTaskForm.style.visibility = 'hidden';
-        })
-
-      })
-      const trashButton = document.createElement('button');
-      trashButton.addEventListener('click', e => {
-        editTaskForm.style.visibility = 'hidden';
-        currTaskRow.remove();
-      })
-
-      currTaskRow.appendChild(checkbox);
-      currTaskRow.innerText = task.value;
-      currTaskRow.appendChild(editButton).innerHTML = "Edit";
-      currTaskRow.appendChild(trashButton).innerText = "Trash";
-      taskDisplay.appendChild(currTaskRow);
-
-      //* Top Line Spacer
+  //       editTaskForm.style.visibility = 'visible'
+  //       const inputField = document.getElementById('taskRenameInputField');
+  //       const submitButton = document.getElementById('cancelButton');
+  //       const cancelButton = document.getElementById('submitButton');
 
 
-      //* Bottom Line Spacer
-      const line = document.createElement('div');
-      line.style.padding = '4px';
-      line.style.borderBottom = '1px solid black';
-      currTaskRow.style.padding = '4px';
-      currTaskRow.appendChild(line);
+  //       // inputField.setAttribute('type', '')
+  //       //Submit Button
+  //       submitButton.innerText = 'Submit';
+  //       cancelButton.innerText = 'cancel';
+  //       submitButton.addEventListener('click', e => {
+
+  //         if (inputField.value) {
+  //           editTaskForm.style.visibility = 'hidden';
+  //           currTaskRow.appendChild(checkbox);
+  //           currTaskRow.innerText = inputField.value;
+  //           currTaskRow.appendChild(editButton).innerHTML = "Edit";
+  //           currTaskRow.appendChild(trashButton).innerText = "Trash";
+  //         } else {
+  //           inputField.placeholder = 'Type an input to submit form'
+  //         }
+
+  //       })
+
+  //       //Cancel Button
+  //       cancelButton.addEventListener('click', e => {
+  //         editTaskForm.style.visibility = 'hidden';
+  //       })
+
+  //     })
+  //     const trashButton = document.createElement('button');
+  //     trashButton.addEventListener('click', e => {
+  //       editTaskForm.style.visibility = 'hidden';
+  //       currTaskRow.remove();
+  //     })
+
+  //     currTaskRow.appendChild(checkbox);
+  //     currTaskRow.innerText = task.value;
+  //     currTaskRow.appendChild(editButton).innerHTML = "Edit";
+  //     currTaskRow.appendChild(trashButton).innerText = "Trash";
+  //     taskDisplay.appendChild(currTaskRow);
+
+  //     //* Top Line Spacer
 
 
-      task.value = "";
-    } else {
-      task.placeholder = "A Task is Required";
-    }
-  })
-
-  //!-------------------------------------------------------------------------------
-  //* List Functionality
-  const listLists = document.getElementById('listLists');
-  const addListButton = document.getElementById('addListBtn');
-
-  addListButton.addEventListener('click', e => {
-    e.preventDefault();
-    const inputField = document.getElementById('addListInput');
-
-    inputField.type = "";
+  //     //* Bottom Line Spacer
+  //     const line = document.createElement('div');
+  //     line.style.padding = '4px';
+  //     line.style.borderBottom = '1px solid black';
+  //     currTaskRow.style.padding = '4px';
+  //     currTaskRow.appendChild(line);
 
 
+  //     task.value = "";
+  //   } else {
+  //     task.placeholder = "A Task is Required";
+  //   }
+  // })
 
-  })
+  // //!-------------------------------------------------------------------------------
+  // //* List Functionality
+  // const listLists = document.getElementById('listLists');
+  // const addListButton = document.getElementById('addListBtn');
+
+  // addListButton.addEventListener('click', e => {
+  //   e.preventDefault();
+  //   const inputField = document.getElementById('addListInput');
+
+  //   inputField.type = "";
+  // })
 
 
 
